2b3c1f3188b17c08b7594c7f6838d1ebe6e83743
Shimon Sant <122385952+shimonsant2@users.noreply.github.com>
2023-01-15 08:04:09 +0200

    Update ML lang italy and amazon interview

--- ex5_languages.py
+++ ex5_languages.py
@@ -4,11 +4,12 @@ openai.api_key_path = "/Users/shimonsant/PycharmProjects/gpt3/key.txt"
 
 response = openai.Completion.create(
     model="text-davinci-003",
-    prompt="Translate this into 1. French, 2. Spanish and 3. Japanese and 4. Hebrew 5. German:\n\nWhat rooms do you "
+    prompt="Translate this into 1. French, 2. Spanish and 3. Japanese and 4. Hebrew 5. German 6. Italy:\n\nWhat rooms "
+           "do you"
            "have"
            "available?\n\n1.",
     temperature=0.3,
-    max_tokens=100,
+    max_tokens=200,
     top_p=1.0,
     presence_penalty=0.0,
     frequency_penalty=0.0
